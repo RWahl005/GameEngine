9463d3d43c08515a4f6c208b4d99cc5b01c416dc
RWahl8177 <rwahl005@west-mec.org>
2019-09-05 13:13:25 -0700

    SceneManagement Test
    Scene Manager test.

---
+++ SceneManager.js
@@ -0,0 +1,14 @@
+class Scene {
+    constructor(){
+        this.listOfObjects = [];
+    }
+    add(obj){
+        
+    }
+    remove(obj){
+
+    }
+    clear(){
+        this.listOfObjects = [];
+    }
+}
